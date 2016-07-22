e50dc908af15b1abf0a5accad9ed1629e67055c5
David Padrino <dpadrino@4geeks.com.ve>
2016-07-21 22:54:43 -0400

    v2

--- app/js/aliadas.js
+++ app/js/aliadas.js
@@ -1,30 +1,33 @@
-$(document).ready(main);
+//$(document).ready(main);
+$(document).ready(function(){
 
-var contador=1;
+	var contador=1;
 
-function main(){
-	// Mostrar y ocultar menú
-	$('.menu-bar').click(function(){
-		if(contador == 1){
-			$('nav').animate({
-				left:'0'
-			});
-			contador = 0;
-		}else{
-			$('nav').animate({
-				left:'-100%'
-			});
-			contador = 1;
-		}
+	//function main(){
+		// Mostrar y ocultar menú
+		$('.menu-bar').click(function(){
+			if(contador == 1){
+				$('nav').animate({
+					left:'0'
+				});
+				contador = 0;
+			}else{
+				$('nav').animate({
+					left:'-100%'
+				});
+				contador = 1;
+			}
 
-	});
+		});
 
-	//Mostrar y ocultar sub-menu
-	$('.submenu').click(function(){
-		//$(this).children('.children').slideToggle();
-		$('.children').slideToggle();
-	});
+		//Mostrar y ocultar sub-menu
+		$('.submenu').click(function(){
+			//$(this).children('.children').slideToggle();
+			$('.children').slideToggle();
+		});
 
 
 
-}
+
+});
+	
